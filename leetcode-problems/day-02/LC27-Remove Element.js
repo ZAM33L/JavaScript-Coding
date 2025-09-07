432545c3b403ba7bad658060e1262c4cb6e807dc
ZAM33L <jameelasferzee@gmail.com>
2025-09-07 12:45:43 +0530

    main

---
+++ leetcode-problems/day-02/LC27-Remove Element.js
@@ -0,0 +1,22 @@
+// var removeElements = function(nums,k) { 
+//     if(nums.length === 0) return 0
+//     nums = nums.filter(item => item !=k)
+//     return nums.length
+// };
+
+var removeElement = function(nums, k) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== k) {
+            nums[i] = nums[j]; 
+            i++;
+        }
+    }
+    
+    return i; 
+};
+
+
+nums = [1,1,2]
+k = 1
+console.log(removeElements(nums,k))
