0c68de19db049fac98c4b38667eca3fd3ed83aac
ZAM33L <jameelasferzee@gmail.com>
2025-09-07 12:48:06 +0530

    main

--- leetcode-problems/day-02/LC35-Search Insert Position.js
+++ leetcode-problems/day-02/LC35-Search Insert Position.js
@@ -12,3 +12,20 @@ nums = [1,3,5,6]
 target = 5
 
 console.log(searchInsert(nums,target))
+
+/*
+var searchInsert = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) return mid;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left; // insertion point
+};
+
+*/
