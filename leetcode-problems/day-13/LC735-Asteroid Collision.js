1e36d30e21b8d183d71a1b79a921145641af7552
JameelAsferZ <MohammedJameelAsferZ@lumel.com>
2025-09-18 10:24:56 +0530

    main

---
+++ leetcode-problems/day-13/LC735-Asteroid Collision.js
@@ -0,0 +1,27 @@
+
+var asteroidCollision = function(asteroids) {
+  let stack = [];
+
+  for (let a of asteroids) {
+    while (
+      stack.length > 0 &&
+      a < 0 &&
+      stack[stack.length - 1] > 0
+    ) {
+      let diff = a + stack[stack.length - 1];
+      if (diff < 0) {
+        stack.pop();
+      } else if (diff > 0) {
+        a = 0;
+      } else {
+        a = 0;
+        stack.pop();
+      }
+    }
+    if (a !== 0) {
+      stack.push(a);
+    }
+  }
+
+  return stack;
+};
